a4dc2b5a2cbfcf9921d0f989cd8cb3c9c88cbf0e
Gulamov Yunis <139101778+Yunis003@users.noreply.github.com>
2025-01-02 17:32:50 +0400

    static-method-learning

--- JavaScript/second/app.js
+++ JavaScript/second/app.js
@@ -21,13 +21,45 @@
 
 //! STATIC METHODS
 
-class User {
-    static sum(a,b){
-        console.log(a+b)
+// class User {
+//     static sum(a,b){
+//         console.log(a+b)
+//     }
+//     divide(a,b){
+//         console.log(a/b)
+//     }
+// }
+// let newUser = new User()
+// User.sum(5,2)
+
+class Persons{
+    constructor(name){
+        this.name = name;
+        console.log(this.name)
     }
-    divide(a,b){
-        console.log(a/b)
+    static checkLength(data){
+        if (data.length <= 5){
+            console.log('err')
+        }else{
+            console.log('ok')
+        }
+    }
+    static checkWordCount(data){
+        const dataLength = data.split(' ');
+        for(let i = 0; i < dataLength.length; i++){
+            if (i < 2){
+                console.log('err')
+            }else{console.log('ok')}
+        }
+    }
+    static setChange(name){
+        console.log('Salam' + ' ' + name)
     }
 }
-let newUser = new User()
-newUser.sum(5,2)
+let person1 = new Persons("Yunis")
+let person2 = new Persons("Telman")
+Persons.setChange("Yunis")
+Persons.checkWordCount('Salam salam salam')
+
+
+
