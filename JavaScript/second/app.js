e0d424cd36a01b3386caa7e345fc950883d72c39
Gulamov Yunis <139101778+Yunis003@users.noreply.github.com>
2025-01-03 21:04:26 +0400

    practice

--- JavaScript/second/app.js
+++ JavaScript/second/app.js
@@ -32,34 +32,133 @@
 // let newUser = new User()
 // User.sum(5,2)
 
-class Persons{
-    constructor(name){
-        this.name = name;
-        console.log(this.name)
+//! another task
+// class Persons{
+//     constructor(name){
+//         this.name = name;
+//         console.log(this.name)
+//     }
+//     static checkLength(data){
+//         if (data.length <= 5){
+//             console.log('err')
+//         }else{
+//             console.log('ok')
+//         }
+//     }
+//     static checkWordCount(data){
+//         const dataLength = data.split(' ');
+//         for(let i = 0; i < dataLength.length; i++){
+//             if (i < 2){
+//                 console.log('err')
+//             }else{console.log('ok')}
+//         }
+//     }
+//     static setChange(name){
+//         console.log('Salam' + ' ' + name)
+//     }
+// }
+// let person1 = new Persons("Yunis")
+// let person2 = new Persons("Telman")
+// Persons.setChange("Yunis")
+// Persons.checkWordCount('Salam salam salam')
+
+//!another simple task
+
+// class Book{
+//     constructor(title, author, pages, isRead){
+//         this.title = title;
+//         this.author = author;
+//         this.pages = pages;
+//         this.isRead = isRead;
+//     }
+//     getSummary(){
+//         console.log(`The book ${this.title} by ${this.author} has ${this.pages} pages`)
+//     }
+//     toggleReadStatus(){
+//         this.isRead = !this.isRead;
+//         console.log(this.isRead)
+//     }
+// }
+// const newBook = new Book('Animal Literature', 'George Orwell', 328, false);
+// console.log(newBook);
+// newBook.getSummary()
+// newBook.toggleReadStatus()
+// newBook.toggleReadStatus()
+
+//! another simple task
+
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     introduce(){
+//         console.log("Hi, my name is " + this.name + ", and I am " + this.age + " " + "years old.")
+//     }
+// }
+// class Student extends Person{
+//     constructor(name, age, grade){
+//         super(name, age);
+//         this.grade = grade;
+//     }
+//     introduce(){
+//         super.introduce()
+//         console.log(" " + "I am in grade" + this.grade + ".");
+//     }
+// }
+// class Teacher extends Person{
+//     constructor(name, age, subject){
+//         super(name, age);
+//         this.subject = subject;
+//     }
+//     introduce(){
+//         super.introduce() 
+//         console.log("I teach" + this.subject + ".");
+//     }
+// }
+// const student = new Student("John", 17, 12);
+// student.introduce();
+
+// const teacher = new Teacher("Sarah", 30, "Mathematics");
+// teacher.introduce();
+
+//! another simple task
+
+class BankAccount{
+    constructor(accountHolder, balance = 0){
+        this.accountHolder = accountHolder;
+        this.balance = balance;
     }
-    static checkLength(data){
-        if (data.length <= 5){
-            console.log('err')
-        }else{
-            console.log('ok')
-        }
+    deposit(amount){
+        balance += amount;
     }
-    static checkWordCount(data){
-        const dataLength = data.split(' ');
-        for(let i = 0; i < dataLength.length; i++){
-            if (i < 2){
-                console.log('err')
-            }else{console.log('ok')}
-        }
+    withdraw(amount){
+        balance -= amount;
     }
-    static setChange(name){
-        console.log('Salam' + ' ' + name)
+    getBalance(){
+        console.log(this.balance);
     }
 }
-let person1 = new Persons("Yunis")
-let person2 = new Persons("Telman")
-Persons.setChange("Yunis")
-Persons.checkWordCount('Salam salam salam')
+
+class SavingsAccount extends BankAccount{
+   constructor(interestRate = 0.05, balance){
+    super(this.balance, this.deposit);
+    this.interestRate = interestRate;
+   }
+   addInterest(interest){
+    this.balance += interest;
+   }
+}
+class CheckingAccount extends BankAccount{
+    constructor(overdraftLimit = 0){
+        this.overdraftLimit = overdraftLimit;
+    }
+}
+const savings = new SavingsAccount("alice", 0, 0.1);
+savings.deposit(1000);
+console.log(savings.getBalance()); //1000
+
+
 
 
 
